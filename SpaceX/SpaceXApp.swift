e30b57919863497777e4ff461ba6d96a40d3fc04
omughrabi <o_mugh@hotmail.com>
2023-08-29 18:10:56 +0200

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Omar on 29.08.23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
