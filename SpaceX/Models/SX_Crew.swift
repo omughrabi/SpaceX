1053218820dc60d1c5854dd99c54d274ee41b126
omughrabi <o_mugh@hotmail.com>
2023-08-30 21:57:47 +0200

    Update models

---
+++ SpaceX/Models/SX_Crew.swift
@@ -0,0 +1,8 @@
+//
+//  SX_Crew.swift
+//  SpaceX
+//
+//  Created by Omar on 30.08.23.
+//
+
+import Foundation
---
+++ SpaceX/Models/SX_Ship.swift
@@ -0,0 +1,8 @@
+//
+//  SX_Ships.swift
+//  SpaceX
+//
+//  Created by Omar on 30.08.23.
+//
+
+import Foundation
